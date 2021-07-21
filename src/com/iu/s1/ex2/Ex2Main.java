7472cccc762048cd13af8b4c63e08374665e3bf5
Daeki <kimdaeki@gmail.com>
2021-07-21 17:35:06 +0900

    1

--- src/com/iu/s1/ex2/Ex2Main.java
+++ src/com/iu/s1/ex2/Ex2Main.java
@@ -5,7 +5,7 @@ public class Ex2Main {
 	public static void main(String[] args) {
 		//Member member = new Member();
 		
-		//member.id ="";
+		//member.id ="";ff
 		
 		//default
 		//member.age=20;
--- src/com/iu/s2/ex1/Member.java
+++ src/com/iu/s2/ex1/Member.java
@@ -7,48 +7,33 @@ public class Member {
 	private int muge;
 	private int ki;
 	
-	
-	//setter
+	public String getId() {
+		return id;
+	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-	//getter
-	public String getId() {
-		return this.id;
-	}
-	
-	
-	
-	
-	//외부 데이터를 멤버변수에 대입
-	//데이터를 입력하는 메서드는 set멤버변수명
-	//setter 
-	public void setAge(int age) {
-		if(age>0 && age <150) {
-			this.age=age;
-		}else {
-			this.age=20;
-		}
-	}
-	
-	//멤버변수를 외부로 보낼때
-	//데이터를 외부로 내보낼때 get멤버변수명
-	//getter
 	public int getAge() {
-		return this.age;
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getMuge() {
+		return muge;
+	}
+	public void setMuge(int muge) {
+		this.muge = muge;
+	}
+	public int getKi() {
+		return ki;
+	}
+	public void setKi(int ki) {
+		this.ki = ki;
 	}
 	
-	public void info() {
-		System.out.println("Id : "+id);
-		System.out.println("Age : "+age);
-		System.out.println("Muge : "+muge);
-		System.out.println("Ki : "+ki);
-		test();
-	}
 	
-	private void test() {
-		
-	}
+	
+	
 
 }
